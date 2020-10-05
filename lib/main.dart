b3f54c55c07d4ba088c0daca714a3eebc1a3598d
Kush Vatsa <kushvatsa007@gmail.com>
2020-10-05 19:31:41 +0200

    android multidex and others

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,35 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:thetamedbot/pages/auth_widget.dart';
+import 'package:thetamedbot/pages/auth_widget_builder.dart';
+import 'package:thetamedbot/services/firebase_auth_service.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'ThetaMedBot',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: HomePage(),
+    return MultiProvider(
+      providers: [
+        Provider<FirebaseAuthService>(
+          create: (_) => FirebaseAuthService(),
+        ),
+      ],
+      child: AuthWidgetBuilder(builder: (context, userSnapshot) {
+        return MaterialApp(
+          title: 'ThetaMedBot',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+          ),
+          home: AuthWidget(userSnapshot: userSnapshot),
+        );
+      }),
     );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
   }
 }
