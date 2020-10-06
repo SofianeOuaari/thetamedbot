b6b21d061853de3f02c1c6c33f9ccd7478d0565f
SofianeOuaari <sofiane.ouaari@gmail.com>
2020-10-06 16:53:58 +0100

    Description Pges

---
+++ lib/authentification/sign_up.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SignUpPage extends StatefulWidget {
+  _SignUpPage createState() => _SignUpPage();
+}
+
+class _SignUpPage extends State<SignUpPage> {
+  Widget build(BuildContext context) {
+    return Scaffold(body: Container());
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:splashscreen/splashscreen.dart';
 import 'main_page.dart';
+import 'description.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,15 +12,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
       theme: ThemeData(
-        
-        primarySwatch: Colors.teal,//Colors.teal,
+        primarySwatch: Colors.teal, //Colors.teal,
         primaryColor: Colors.teal[100],
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: SplashScreen(seconds: 15,navigateAfterSeconds: MainPage(),image: Image.asset("assets/logo_thetamedbot_text.png") ,backgroundColor: Color(0xffe3fdfd),photoSize: 130,),
-    
+      home: SplashScreen(
+        seconds: 15,
+        navigateAfterSeconds: DescriptionPage()/*MainPage()*/,
+        
+        image: Image.asset("assets/logo_thetamedbot_text.png"),
+        backgroundColor: Color(0xffe3fdfd),
+        photoSize: 130,
+      ),
     );
   }
 }
