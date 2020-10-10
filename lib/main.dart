85acf159cd664e50adad6ffce5ef3e09c4f975e0
Kush Vatsa <kushvatsa007@gmail.com>
2020-10-10 13:36:04 +0200

    removed splash from authwidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:splashscreen/splashscreen.dart';
 import 'package:provider/provider.dart';
 import 'package:thetamedbot/pages/auth_widget.dart';
 import 'package:thetamedbot/pages/auth_widget_builder.dart';
+import 'package:thetamedbot/pages/home_page/home_page.dart';
+import 'package:thetamedbot/pages/signin_page/signin_page.dart';
+import 'package:thetamedbot/pages/signin_page/signup_page.dart';
 import 'package:thetamedbot/services/firebase_auth_service.dart';
 
 void main() async {
@@ -12,8 +14,13 @@ void main() async {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final navigatorKey = GlobalKey<NavigatorState>();
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
@@ -24,19 +31,20 @@ class MyApp extends StatelessWidget {
       ],
       child: AuthWidgetBuilder(builder: (context, userSnapshot) {
         return MaterialApp(
+          navigatorKey: navigatorKey,
           title: 'ThetaMedBot',
           theme: ThemeData(
             primarySwatch: Colors.blue,
             visualDensity: VisualDensity.adaptivePlatformDensity,
           ),
-          home: SplashScreen(
-            seconds: 15,
-            navigateAfterSeconds:
-                AuthWidget(userSnapshot: userSnapshot) /*MainPage()*/,
-            image: Image.asset("assets/logo_thetamedbot_text.png"),
-            backgroundColor: Color(0xffe3fdfd),
-            photoSize: 130,
+          home: AuthWidget(
+            userSnapshot: userSnapshot,
           ),
+          routes: <String, WidgetBuilder>{
+            "/home": (context) => HomePage(userSnapshot: userSnapshot),
+            "/sin": (context) => SignInPage(),
+            "/sup": (context) => SignUpPage(),
+          },
         );
       }),
     );
--- lib/pages/auth_widget.dart
+++ lib/pages/auth_widget.dart
@@ -3,40 +3,18 @@ import 'package:flutter/material.dart';
 import 'package:thetamedbot/models/myuser.dart';
 import 'package:thetamedbot/pages/home_page/home_page.dart';
 import 'package:thetamedbot/pages/signin_page/signin_page.dart';
-import 'package:thetamedbot/pages/splash_x.dart';
-import 'package:thetamedbot/services/splash_service.dart';
 
 class AuthWidget extends StatelessWidget {
-  const AuthWidget(
-      {Key key, @required this.userSnapshot, @required this.splashService})
-      : super(key: key);
+  const AuthWidget({Key key, @required this.userSnapshot}) : super(key: key);
   final AsyncSnapshot<MyUser> userSnapshot;
-  final SplashService splashService;
 
   @override
   Widget build(BuildContext context) {
-    final splash = splashService.isFirst;
     if (userSnapshot.connectionState == ConnectionState.active) {
       if (userSnapshot.hasData) {
-        if (splash) {
-          return HomePage(userSnapshot: userSnapshot);
-        } else {
-          return SplashX(
-            wx: "/home",
-            splashService: splashService,
-            ctx: context,
-          );
-        }
+        return HomePage(userSnapshot: userSnapshot);
       } else {
-        if (splash) {
-          return SignInPage();
-        } else {
-          return SplashX(
-            wx: "/sin",
-            splashService: splashService,
-            ctx: context,
-          );
-        }
+        return SignInPage();
       }
     }
     return Scaffold(
